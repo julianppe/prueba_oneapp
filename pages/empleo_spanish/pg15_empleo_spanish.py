adf68f8031fe1296d0822c5fb73e77ba3d0035b4
julianppe <72886831+julianppe@users.noreply.github.com>
2022-12-15 15:52:10 -0300

    cambio \ por /

--- pages/empleo_spanish/pg15_empleo_spanish.py
+++ pages/empleo_spanish/pg15_empleo_spanish.py
@@ -14,7 +14,7 @@ dash.register_page(__name__,
 
 
 # page 1 data
-df = pd.read_csv("datasets\empleo_spanish/calificacion_alta.csv")
+df = pd.read_csv("datasets/empleo_spanish/calificacion_alta.csv")
 df['indicador'] = df['indicador'].astype(str)
 df['pais'] = df['pais'].astype(str)
 df['comparacion_por'] = df['comparacion_por'].astype(str)
