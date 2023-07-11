9c3e7fa784f517848239e6956db4e77edc6f0ac4
julianppe <72886831+julianppe@users.noreply.github.com>
2023-07-11 18:14:03 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.read_csv("datasets/empleo_spanish/participacion.csv")
 df['pais'] = df['pais'].astype(str)
 app_spanning_input = dcc.Dropdown(
-    options=[{'label': x, 'value': x} for x in df.pais.unique()],
+    options=['Argentina','Bolivia', 'Brasil'],
     id="all-pages-pais",
     multi=True, 
     value="Argentina", 
