ed4eba8a97d97de577fbe4f751957278d70f3010
julianppe <72886831+julianppe@users.noreply.github.com>
2023-07-12 09:52:42 -0300

    cambios dropdown

--- pages/empleo_spanish/pg14_empleo_spanish.py
+++ pages/empleo_spanish/pg14_empleo_spanish.py
@@ -3,7 +3,7 @@ from dash import dcc, html, callback, Output, Input
 import plotly.express as px
 import dash_bootstrap_components as dbc
 import pandas as pd
-import utils
+from elements.elements_empleo_spanish import dropdown_pais_empleo_spanish
 from dash import dcc, html, register_page, ctx, no_update
 from dash_extensions.enrich import Output, Input, State, callback
 
@@ -39,7 +39,7 @@ list_comparacion_por_ordenada = [x for _,x in sorted(zip(list_comparacion_por_or
 layout = html.Div([
         dbc.Row([
         dbc.Col([
-                        utils.app_spanning_input,
+            dropdown_pais_empleo_spanish,
         ], width=6),
         dbc.Col([
             dcc.Dropdown(options=[{'label': x, 'value': x} for x in list_comparacion_por_ordenada], multi=False, persistence=True, persistence_type='memory', value='Total', id='page14_empleo_spanish-comparacion_por_elect')
